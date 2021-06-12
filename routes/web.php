84bbf0d07f11d410169b7a3f48509299714394e0
A . S . M . RADWAN <50805604+radwanromy@users.noreply.github.com>
2021-06-12 14:58:35 +0600

    bingo

--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,15 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+/*
 Route::get('/', function () {
     return view('welcome');
 });
+*/
 
+Route::get('/', function () {
+    return view('pages.index');
+});
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
